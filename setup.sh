7b3b388efaa44fbce5cc70c008516a64f0cfdd19
Georgii Ishchenko <ishchenko.gv@gmail.com>
2025-11-08 00:18:52 +0000

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 mkdir -vp ~/.config
 
-echo "✨ Checking shell..."
-
 if ! command -v zsh &>/dev/null; then
   echo "ℹ️ zsh not found, installing..."
   brew install zsh
@@ -11,7 +9,7 @@ if ! command -v zsh &>/dev/null; then
   echo "Changing default shell"
   chsh -s $(which zsh)
 
-  echo "Setup zsh config"
+  echo "Linking zsh config"
   mv ~/.zshrc ~/.zshrc.bak
   ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
@@ -20,8 +18,6 @@ if ! command -v zsh &>/dev/null; then
 else
   echo "✅ zsh found"
 fi
-
-echo "✨ Checking dependencies..."
 
 if ! command -v luarocks &>/dev/null; then
   echo "ℹ️ luarocks not found, installing..."
@@ -34,20 +30,40 @@ if ! command -v rg &>/dev/null; then
   echo "ℹ️ rg not found, installing..."
   brew install ripgrep
 else
-  echo "✅ ripgrep found"
+  echo "✅ rg found"
 fi
 
-echo "✨ Checking symlinks..."
+if ! brew list --cask aerospace &>/dev/null; then
+  echo "ℹ️ aerospace not found, installing..."
+  brew install --cask nikitabobko/tap/aerospace
+  ln -s ~/.dotfiles/aerospace.toml ~/.config/aerospace/aerospace.toml
+else
+  echo "✅ aerospace found"
+fi
+
+if ! brew list nvim &>/dev/null; then
+  echo "ℹ️ nvim not found, installing..."
+  brew install nvim
+else
+  echo "✅ nvim found"
+fi
 
 if [ ! -e "$HOME/.config/nvim" ]; then
-  echo "ℹ️ ~/.config/nvim/ not found, creating symlink..."
+  echo "ℹ️ ~/.config/nvim/ not found, linking config..."
   ln -s ~/.dotfiles/nvim/ ~/.config/nvim/
 else
   echo "✅ ~/.config/nvim/ found"
 fi
 
+if ! brew list tmux &>/dev/null; then
+  echo "ℹ️ tmux not found, installing..."
+  brew install tmux
+else
+  echo "✅ tmux found"
+fi
+
 if [ ! -e "$HOME/.config/tmux" ]; then
-  echo "ℹ️ ~/.config/tmux/ not found, creating symlink..."
+  echo "ℹ️ ~/.config/tmux/ not found, linkink config..."
   mkdir -vp ~/.config/tmux/ && ln -s ~/.dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 else
   echo "✅ ~/.config/tmux/ found"
