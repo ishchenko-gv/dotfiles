408be4275e2016524570bf0712e2fc695a7a753b
Georgii Ishchenko <ishchenko.gv@gmail.com>
2026-05-17 00:35:44 +0100

    cleanup

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -13,24 +13,9 @@ return {
 		local cmp = require("cmp")
 		cmp.setup({
 			snippet = {
-				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-					-- For `mini.snippets` users:
-					-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-					-- insert({ body = args.body }) -- Insert at cursor
-					-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-					-- require("cmp.config").set_onetime({ sources = {} })
+					require("luasnip").lsp_expand(args.body)
 				end,
-			},
-			window = {
-				-- completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -41,26 +26,11 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				-- { name = 'vsnip' }, -- For vsnip users.
 				{ name = "luasnip" }, -- For luasnip users.
-				-- { name = 'ultisnips' }, -- For ultisnips users.
-				-- { name = 'snippy' }, -- For snippy users.
 			}, {
 				{ name = "buffer" },
 			}),
 		})
-
-		-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-		-- Set configuration for specific filetype.
-		--[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]]
-		--
 
 		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline({ "/", "?" }, {
@@ -83,7 +53,6 @@ return {
 
 		-- Set up lspconfig.
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 		local servers = { "gopls", "stylua" }
 		for _, s in ipairs(servers) do
 			vim.lsp.config(s, {
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,9 @@
 return {
-	{ "mason-org/mason.nvim" },
-	{ "mason-org/mason-lspconfig.nvim" },
-	{ "neovim/nvim-lspconfig" },
+	{
+		"mason-org/mason.nvim",
+		dependencies = {
+			"mason-org/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+	},
 }
